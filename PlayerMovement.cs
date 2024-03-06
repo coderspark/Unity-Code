e90500bdb43a2ed7b17b8c9de2facf1968a2b50e
CoderSpark <115105008+coderspark@users.noreply.github.com>
2024-03-06 18:28:53 +0100

    Created PlayerMovement.cs

---
+++ PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Main movement variables
+    public float movespeed = 9f;
+    public float jumppower;
+    public bool grounded;
+
+    // GameObjects, Transforms, and more of that stuff
+    public Transform tr;
+    public Rigidbody rb;
+    public Transform level;
+    public GameObject cam;
+
+    // Other things to keep code clean
+    public float distance;
+
+    // movement
+    void Movement()
+    {
+        movestate = "Idle";
+        if (Input.GetKey(KeyCode.D))
+        {
+            tr.Translate((Vector3.right * movespeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            tr.Translate((Vector3.left * movespeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            tr.Translate((Vector3.forward * movespeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            tr.Translate((Vector3.back * movespeed) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(Vector3.up * jumppower);
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set ground to true/false
+        distance = Vector3.Distance(level.position, tr.position);
+        if (distance < 1.5f)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+        Movement();
+        
+    }
+}
