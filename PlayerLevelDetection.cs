acf87a63da6025d808eac9fcc62ac844973efc30
CoderSpark <115105008+coderspark@users.noreply.github.com>
2024-03-11 11:29:06 +0100

    Update PlayerLevelDetection.cs

--- PlayerLevelDetection.cs
+++ PlayerLevelDetection.cs
@@ -1,17 +1,1 @@
-using UnityEngine;
 
-public class PlayerLevelDetection : MonoBehaviour
-{
-    public Transform player;
-    private Vector3 loc;
-    RaycastHit hit;
-    // Update is called once per frame
-    void Update()
-    {   
-        if (Physics.Raycast(player.position, Vector3.down, out hit))
-        {
-            loc = hit.point;
-        }
-        transform.position = new Vector3(player.position.x , loc.y - 0.49f, player.position.z);
-    }
-}
